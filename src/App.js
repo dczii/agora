193d41290d5dcb37e2a9ffd2dd63107628fa4ddd
dczii <dczii@live.com>
2020-09-17 14:43:50 +0800

    Add authentication.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,91 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Auth, Hub } from 'aws-amplify'
+import { Authenticator, AmplifyTheme } from 'aws-amplify-react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import ProfilePage from './pages/ProfilePage'
+import MarketPage from './pages/MarketPage'
+import Navbar from './components/Navbar'
+
 import "./App.css";
 
-class App extends React.Component {
-  state = {};
+function App() {
+  const [user, setUser] = useState(null)
+  
+  useEffect(() => {
+    getUserData();
+    Hub.listen('auth', onHubCapsule)
+  })
 
-  render() {
-    return <div>App</div>;
+  const getUserData = async () => {
+    const user = await Auth.currentAuthenticatedUser()
+    if(user) setUser(user)
+    else setUser(null)
+  }
+
+  const onHubCapsule = capsule => {
+    switch(capsule.payload.event) {
+      case 'signIn':
+        console.log('sign-in')
+        getUserData()
+        break;
+      case 'signUp':
+        console.log('signed-up')
+        break;
+      case 'signOut':
+        console.log('signout')
+        setUser(null)
+        break;
+      default:
+        return;
+    }
+  }
+  
+  const handleSignOut = async () => {
+    try {
+      await Auth.signOut()
+      setUser(null)
+    } catch(error) {
+      console.log('Signing out user -',error)
+    }
+  }
+  
+  return !user ? (
+    <Authenticator theme={theme} /> 
+  ) : (
+    <Router>
+      <>
+        {/* Navigation Bar */}
+        <Navbar user={user} signOut={handleSignOut}/>
+      
+        {/* Routes */}
+        <div className="app-container">
+          <Route exact path='/' component={HomePage}/>
+          <Route path='/profile' component={ProfilePage}/>
+          <Route path='/markets/:marketId' component={({ match }) => <MarketPage marketId={match.params.marketId} /> }/>
+        </div>
+      </>
+    </Router>
+  );
+}
+
+const theme = {
+  ...AmplifyTheme,
+  navBar: {
+    ...AmplifyTheme.navBar,
+    backgroundColor: 'var(--lightAmazonOrange)'
+  },
+  button: {
+    ...AmplifyTheme.button,
+    backgroundColor: 'var(--amazonOrange)'
+  },
+  sectionBody: {
+    ...AmplifyTheme.sectionBody,
+    padding: '5px'
+  },
+  sectionHeader: {
+    ...AmplifyTheme.sectionHeader,
+    backgroundColor: 'var(--squidInk)'
   }
 }
 
--- src/pages/MarketPage.js
+++ src/pages/MarketPage.js
@@ -1,12 +1,8 @@
 import React from "react";
 // import { Loading, Tabs, Icon } from "element-react";
 
-class MarketPage extends React.Component {
-  state = {};
-
-  render() {
-    return <div>MarketPage</div>;
-  }
+function MarketPage({ marketId }) {
+  return <div>MarketPage - {marketId}</div>;
 }
 
 export default MarketPage;
